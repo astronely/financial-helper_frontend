d91e33ffba832fd07c7a6541885ea4bc10d2c156
astronely <nikborowik200394@gmail.com>
2025-06-12 02:37:07 +0500

    add https for preview

--- financialHelper/vite.config.js
+++ financialHelper/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path';
 import { fileURLToPath } from 'url';
+import * as fs from "node:fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,6 +14,10 @@ export default defineConfig({
     port: 5173,
     strictPort: true,
     host: '0.0.0.0',
+    https: {
+      key: fs.readFileSync('/app/certs/key.pem'),
+      cert: fs.readFileSync('/app/certs/cert.pem'),
+    },
     proxy: {
       '/api': {
         target: 'http://api-gateway:8081',
